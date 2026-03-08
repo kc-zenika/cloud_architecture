56f54bb24a389be7a9cde3f9db7b7174c33026a7
kc <kokchoong611@gmail.com>
2026-03-08 23:29:23 +0800

    feat: add some steps in 1.2_vpc.md

--- working-example/main.tf
+++ working-example/main.tf
@@ -1,1 +1,21 @@
-# TODO
+terraform {
+  required_version = ">= 1.0"
+
+  # local provider
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67"
+    }
+  }
+
+  # backend
+  backend "s3" {
+    bucket         = "workshop-tfstate-787525931078"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
