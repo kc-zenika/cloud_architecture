59c2d89e98e897ed13d06a3014f4fa231105f54a
kc <kokchoong611@gmail.com>
2026-03-22 12:15:25 +0800

    feat: init workshop part 1

--- README.md
+++ README.md
@@ -1,92 +1,15 @@
 # Cloud Architecture
 
 ## Part 1 (5 hours)
-### Intro (45 mins)
-- mainly on AWS
-- Goal: to create a set of maintainable/evolvable infra
-  - day to day tasks of a platform engineer
-  - a set of AWS resources to a public app
-  - some system design challenges
-- define what we are going to do
-- perpare an environment thats fully ready
-  - Permissions - RBAC and IBAC
-  - show traffic flow diagram ingress (igw -> public subnet -> public ip -> private -> EC2)
-  - ec2 app
-- participants to follow along with instructions in notes
-  - mainly steps/links
-  - working-example is the answer incase they cant follow 
-### Handson Tooling (30 mins)
-- `.aws/config`
-  - authenticate to sandbox aws account
-  - talk about multi account
-- basic terraform and IAC
-  - simple terraform create a file with `local_file` to show its not just for AWS
-  - some buffer time to get everyone installed correctly
-  - what is a provider and backend, etc
-- setup backend on s3 + dynamodb
-### Handson VPC (60 mins)
-- create a VPC
-- subnet segmentations best practices (2 public, 2 private for now)
-- security groups vs NACLs
-### Handson Basic app + design discussions (120 mins - to the end)
-- TODO
-  - need some planned gotchas where participants can troubleshoot
-- provision IAM role for execution role/instance profile
-- provision an EC2 in private subnet
-  - Just use AL2023 first, but show selection
-    - TODO concept of cattle not pets? Immutable infra
-    - talk about decisions on selecting this
-    - what you do in prod (golden AMI)
-  - selection of instance types (some ballparks)
-  - storage size (encryption)
-  - prep user data, bootstraping script
-    - just runs nginx with a simple index.html
-  - show that it cant install packages
-    - create the NAT/igw to fix the bootstraping step
-- Access using session manager
-  - show it can only be accessed in private subnet
-  - show metadata `curl 169.254.169.254/latest/meta-data`
-  - show how its insecure to put keys in userdata cause its exposed in metadata
-- provision EIP in public subnet to expose app
-  - should be able to hit the index.html now
-### Ending Topics/Discussions
-- what else is missing before we can go to prod? (For part 2 and 3)
-- if theres more time
-  - ASG and show some scaling?
-  - R53 and Cloudfront? - show some diagrams/homework
+- Workshop materials
+  - [Intro, Architecure, & Terraform](notes/1.1_intro.md)
+  - [VPC & Networking](notes/1.2_vpc.md)
+  - [IAM](notes/1.3_iam.md)
+  - [Provisioning the app](notes/1.4_provisioning.md)
+- Preface
+  - step-by-step, hands-on creation of AWS resources from scratch
+  - a little debugging, best-practices, and trade-off discussions
+  - a little terraform magic
+  - a few too many ducks 🦆 involved
+  - a lot of networking and security
 
-
-## Part 2 (DRAFT)
-- Reprovision everything in part 1
-- introduce modules in terraform
-  - IAC modules, state segregation
-- potential topics
-  - asg - multiaz availablility, scaling/replicas
-  - alb/nlb
-  - efs/ebs
-  - s3 - KMS/bucket key
-  - waf, network firewall
-  - cloudfront - cache
-  - params and secret
-  - cloudwatch - LGTM - logs, traces, metrics
-  - R53
-- Topic 1: database
-  - deploy an aurora in private DB subnet
-- Topic 2: secrets management
-  - connect to database from the app
-  - updates to IAM auth and networking to make it work 
-- Topic 3: Reliability
-  - talk about multiaz availablility, scaling/replicas
-  - healthchecks
-  - backups / DR
-- Topic 4: logging/monitoring
-  - LGTM - logs, traces, metrics
-  - configure app to push to cloudwatch
-  - some Cloudwatch alarms
-
-## Part 3 (DRAFT)
-- Topic 1: WAF 
-- Topic 2: Security eco-system
-  - configs, inspector, security hub, guard duty, cloudtrail
-- Topic 4: Cost awareness?
-- Something todo with CD?
--- working-example/main.tf
+++ working-example/main.tf
@@ -1,11 +1,15 @@
+####################
+###### Part 1 ######
+####################
+
 terraform {
   required_version = ">= 1.0"
 
-  # local provider
+  # aws provider
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67"
+      version = "6.38"
     }
   }
 
@@ -19,3 +23,502 @@ terraform {
   }
 }
 
+####################
+###### Part 2 ######
+####################
+
+locals {
+  public_subnets = {
+    "ap-southeast-1a" = "10.0.0.0/22"
+    "ap-southeast-1b" = "10.0.4.0/22"
+    "ap-southeast-1c" = "10.0.8.0/22"
+  }
+
+  private_subnets = {
+    "ap-southeast-1a" = "10.0.32.0/19"
+    "ap-southeast-1b" = "10.0.64.0/19"
+    "ap-southeast-1c" = "10.0.96.0/19"
+  }
+}
+
+# 2.3 Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-main"
+    Environment = "dev"
+  }
+}
+
+
+# 2.4 Adding subnets
+
+## Manualy adding 1 by 1
+# resource "aws_subnet" "public_1a" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.0.0/22"
+#   availability_zone = "ap-southeast-1a"
+#   tags = {
+#     Name = "public-1a"
+#     Environment = "dev"
+#   }
+# }
+# ...
+
+resource "aws_subnet" "public" {
+  for_each = local.public_subnets
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block        = each.value
+
+  tags = {
+    Name        = "public-${each.key}"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "private" {
+  for_each = local.private_subnets
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block        = each.value
+
+  tags = {
+    Name        = "private-${each.key}"
+    Environment = "dev"
+  }
+}
+
+
+# 2.5 Add IGW for internet reachability
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "igw-main"
+    Environment = "dev"
+  }
+}
+
+
+# 2.6 Route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "rt-public"
+    Environment = "dev"
+  }
+}
+
+## Manualy adding 1 by 1
+# resource "aws_route_table_association" "public_1a" {
+#   subnet_id      = aws_subnet.public["ap-southeast-1a"].id
+#   route_table_id = aws_route_table.public.id
+# }
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# 2.7 - NACLs - public and private
+locals {
+  nacl_rules = {
+    public = {
+      ingress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 80, to_port = 80 },
+        { rule_no = 110, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+      ]
+      egress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 80, to_port = 80 },
+        { rule_no = 110, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+      ]
+    }
+    private = {
+      ingress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "10.0.0.0/16", from_port = 80, to_port = 80 },
+        { rule_no = 110, protocol = "tcp", action = "allow", cidr_block = "10.0.0.0/16", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+      ]
+      egress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 80, to_port = 80 },
+        { rule_no = 110, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "10.0.0.0/16", from_port = 1024, to_port = 65535 }
+      ]
+    }
+  }
+}
+
+resource "aws_network_acl" "public" {
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [for s in aws_subnet.public : s.id]
+
+  dynamic "ingress" {
+    for_each = local.nacl_rules.public.ingress
+    content {
+      protocol   = ingress.value.protocol
+      rule_no    = ingress.value.rule_no
+      action     = ingress.value.action
+      cidr_block = ingress.value.cidr_block
+      from_port  = ingress.value.from_port
+      to_port    = ingress.value.to_port
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.nacl_rules.public.egress
+    content {
+      protocol   = egress.value.protocol
+      rule_no    = egress.value.rule_no
+      action     = egress.value.action
+      cidr_block = egress.value.cidr_block
+      from_port  = egress.value.from_port
+      to_port    = egress.value.to_port
+    }
+  }
+
+  tags = { 
+    Name = "nacl-public",
+    Environment = "dev"
+  }
+}
+
+resource "aws_network_acl" "private" {
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [for s in aws_subnet.private : s.id]
+
+  dynamic "ingress" {
+    for_each = local.nacl_rules.private.ingress
+    content {
+      protocol   = ingress.value.protocol
+      rule_no    = ingress.value.rule_no
+      action     = ingress.value.action
+      cidr_block = ingress.value.cidr_block
+      from_port  = ingress.value.from_port
+      to_port    = ingress.value.to_port
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.nacl_rules.private.egress
+    content {
+      protocol   = egress.value.protocol
+      rule_no    = egress.value.rule_no
+      action     = egress.value.action
+      cidr_block = egress.value.cidr_block
+      from_port  = egress.value.from_port
+      to_port    = egress.value.to_port
+    }
+  }
+
+  tags = { 
+    Name = "nacl-private",
+    Environment = "dev"
+  }
+}
+
+# 2.8 Security groups
+
+## security group for alb
+resource "aws_security_group" "alb" {
+  name        = "alb"
+  description = "Public ALB Security Group"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_in_http" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_in_https" {
+  security_group_id = aws_security_group.alb.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_out_app_http" {
+  security_group_id = aws_security_group.alb.id
+  referenced_security_group_id = aws_security_group.app.id
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_out_app_https" {
+  security_group_id = aws_security_group.alb.id
+  referenced_security_group_id = aws_security_group.app.id
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+
+## security group for app
+resource "aws_security_group" "app" {
+  name        = "app"
+  description = "Private App Security Group"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_in_http" {
+  security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.alb.id
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_in_https" {
+  security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.alb.id
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "app_out_https" {
+  security_group_id = aws_security_group.app.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+
+####################
+###### Part 3 ######
+####################
+
+# 3.3 IAM policy
+data "aws_iam_policy_document" "app_policy" {
+  statement {
+    sid = "AllowS3Download"
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "arn:aws:s3:::daas-787525931078",
+			"arn:aws:s3:::daas-787525931078/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "app_policy" {
+  name   = "DaaSAppPolicy"
+  description = "Permissions for the DaaS application"
+  policy = data.aws_iam_policy_document.app_policy.json
+}
+
+
+# 3.4 IAM Role
+data "aws_iam_policy_document" "app_role_trust_policy" {
+  statement {
+    sid = "AllowEC2ServiceToAssumeRole"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "app_role" {
+  name               = "DaaSAppRole"
+  assume_role_policy = data.aws_iam_policy_document.app_role_trust_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "app_policy_attach" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = aws_iam_policy.app_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "app_ssm_attach" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# 3.5 EC2 instance profile
+resource "aws_iam_instance_profile" "app_instance_profile" {
+  name = "DaaSInstanceProfile"
+  role = aws_iam_role.app_role.name
+}
+
+# 3.6 Interface VPC endpoint for SSM
+locals {
+  ssm_services = ["ssm", "ssmmessages", "ec2messages"]
+}
+
+resource "aws_security_group" "ssm_vpce_sg" {
+  name        = "ssm-vpce"
+  description = "Allow nodes to talk to SSM Endpoints"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssm_vpce_in_https" {
+  security_group_id = aws_security_group.ssm_vpce_sg.id
+  referenced_security_group_id = aws_security_group.app.id
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "ssm_vpce_out_https" {
+  security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.ssm_vpce_sg.id
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+}
+
+resource "aws_vpc_endpoint" "ssm_endpoints" {
+  # creates 3 vpc endpoints required for ssm
+  for_each = toset(local.ssm_services)
+
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-southeast-1.${each.key}"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [for s in aws_subnet.private : s.id]
+
+  # This allows the 'ssm.ap-southeast-1.amazonaws.com' URL 
+  # to resolve to the private endpoint IP
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.ssm_vpce_sg.id]
+}
+
+
+####################
+###### Part 4 ######
+####################
+
+# EC2
+resource "aws_instance" "web_server" {
+  ami           = "ami-0135b39e8099fe53c"
+  instance_type = "t3.micro"
+
+  user_data = file("user_data.sh")
+  subnet_id = aws_subnet.private["ap-southeast-1a"].id
+
+  iam_instance_profile = aws_iam_instance_profile.app_instance_profile.name
+  vpc_security_group_ids = [aws_security_group.app.id]
+}
+
+# NAT
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public["ap-southeast-1a"].id
+
+  tags = {
+    Name = "nat-main"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+# Route table in private subnets to NAT for internet outbound
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = { Name = "rt-private" }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each       = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
+
+
+# Gateway VPCe for S3
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.ap-southeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  # This automatically injects the S3 route into these route tables
+  route_table_ids = [aws_route_table.private.id]
+}
+
+# ALB
+resource "aws_lb" "app_alb" {
+  name               = "app-alb-internet"
+  internal           = false # internet-facing
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/index.html"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_app_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_server.id
+  port             = 80
+}
+
+# ALB listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
