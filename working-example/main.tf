6d09d4a7d8191bc8f174c6bc35016852162ecefc
kc <kokchoong611@gmail.com>
2026-03-29 12:27:15 +0800

    feat: add notes for route table and nacl

--- working-example/main.tf
+++ working-example/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67"
+      version = "6.38"
     }
   }
 
@@ -34,8 +34,10 @@ locals {
   }
 }
 
+####################
+###### Part 2 ######
+####################
 
-# Part 2
 # 2.3 Create a VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
@@ -51,6 +53,7 @@ resource "aws_vpc" "main" {
 
 # 2.4 Adding subnets
 
+## Manualy adding 1 by 1
 # resource "aws_subnet" "public_1a" {
 #   vpc_id     = aws_vpc.main.id
 #   cidr_block = "10.0.0.0/22"
@@ -97,3 +100,132 @@ resource "aws_internet_gateway" "main" {
     Environment = "dev"
   }
 }
+
+
+# 2.6 Route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "rt-public"
+    Environment = "dev"
+  }
+}
+
+## Manualy adding 1 by 1
+# resource "aws_route_table_association" "public_1a" {
+#   subnet_id      = aws_subnet.public["ap-southeast-1a"].id
+#   route_table_id = aws_route_table.public.id
+# }
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# 2.7 - NACLs - public and private
+locals {
+  nacl_rules = {
+    public = {
+      ingress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+      ]
+      egress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
+      ]
+    }
+    private = {
+      ingress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "10.0.0.0/16", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "10.0.0.0/16", from_port = 1024, to_port = 65535 }
+      ]
+      egress = [
+        { rule_no = 100, protocol = "tcp", action = "allow", cidr_block = "0.0.0.0/0", from_port = 443, to_port = 443 },
+        { rule_no = 120, protocol = "tcp", action = "allow", cidr_block = "10.0.0.0/16", from_port = 1024, to_port = 65535 }
+      ]
+    }
+  }
+}
+
+resource "aws_network_acl" "public" {
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [for s in aws_subnet.public : s.id]
+
+  dynamic "ingress" {
+    for_each = local.nacl_rules.public.ingress
+    content {
+      protocol   = ingress.value.protocol
+      rule_no    = ingress.value.rule_no
+      action     = ingress.value.action
+      cidr_block = ingress.value.cidr_block
+      from_port  = ingress.value.from_port
+      to_port    = ingress.value.to_port
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.nacl_rules.public.egress
+    content {
+      protocol   = egress.value.protocol
+      rule_no    = egress.value.rule_no
+      action     = egress.value.action
+      cidr_block = egress.value.cidr_block
+      from_port  = egress.value.from_port
+      to_port    = egress.value.to_port
+    }
+  }
+
+  tags = { 
+    Name = "nacl-public",
+    Environment = "dev"
+  }
+}
+
+resource "aws_network_acl" "private" {
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [for s in aws_subnet.private : s.id]
+
+  dynamic "ingress" {
+    for_each = local.nacl_rules.private.ingress
+    content {
+      protocol   = ingress.value.protocol
+      rule_no    = ingress.value.rule_no
+      action     = ingress.value.action
+      cidr_block = ingress.value.cidr_block
+      from_port  = ingress.value.from_port
+      to_port    = ingress.value.to_port
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.nacl_rules.private.egress
+    content {
+      protocol   = egress.value.protocol
+      rule_no    = egress.value.rule_no
+      action     = egress.value.action
+      cidr_block = egress.value.cidr_block
+      from_port  = egress.value.from_port
+      to_port    = egress.value.to_port
+    }
+  }
+
+  tags = { 
+    Name = "nacl-private",
+    Environment = "dev"
+  }
+}
+
+####################
+###### Part 3 ######
+####################
+
