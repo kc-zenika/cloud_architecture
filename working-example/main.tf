214d3ab608598bea9b5fce6e1138d01b7af42d08
kc <kokchoong611@gmail.com>
2026-04-26 23:28:07 +0800

    add remaining ALB expose

--- working-example/main.tf
+++ working-example/main.tf
@@ -477,3 +477,48 @@ resource "aws_vpc_endpoint" "s3" {
   # This automatically injects the S3 route into these route tables
   route_table_ids = [aws_route_table.private.id]
 }
+
+# ALB
+resource "aws_lb" "app_alb" {
+  name               = "app-alb-internet"
+  internal           = false # internet-facing
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/index.html"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_app_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_server.id
+  port             = 80
+}
+
+# ALB listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
--- working-example/user_data.sh
+++ working-example/user_data.sh
@@ -11,5 +11,12 @@ systemctl enable nginx
 # Note: This requires an IAM Instance Profile attached to the EC2
 aws s3 cp s3://daas-787525931078/index.html /usr/share/nginx/html/index.html
 
-# 4. Set correct permissions
+# 4. Replace INSTANCE_ID_SED with actual instance ID (IMDSv2)
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
+sed -i "s/INSTANCE_ID_SED/$INSTANCE_ID/g" /usr/share/nginx/html/index.html
+
+# 5. Set correct permissions
 chmod 644 /usr/share/nginx/html/index.html
