a3a72da11454cd1ad66167b6a8c8542186f9ac62
kc <kokchoong611@gmail.com>
2026-07-24 10:51:22 +0800

    fix: precreate alb and ec2 sec groups

--- aws/development/alb.tf
+++ aws/development/alb.tf
@@ -1,1 +1,22 @@
 # create ALB and security groups
+resource "aws_security_group" "alb" {
+  name        = "part2-alb"
+  description = "Public ALB Security Group"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_in_http" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_out_app" {
+  security_group_id            = aws_security_group.alb.id
+  referenced_security_group_id = aws_security_group.app.id
+  ip_protocol                  = "tcp"
+  from_port                    = 8080
+  to_port                      = 8080
+}
--- aws/development/ec2-security-group.tf
+++ aws/development/ec2-security-group.tf
@@ -5,13 +5,13 @@ resource "aws_security_group" "app" {
   vpc_id      = aws_vpc.main.id
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "app_in_http" {
-#   security_group_id            = aws_security_group.app.id
-#   referenced_security_group_id = aws_security_group.alb.id
-#   ip_protocol                  = "tcp"
-#   from_port                    = 8080
-#   to_port                      = 8080
-# }
+resource "aws_vpc_security_group_ingress_rule" "app_in_http" {
+  security_group_id            = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.alb.id
+  ip_protocol                  = "tcp"
+  from_port                    = 8080
+  to_port                      = 8080
+}
 
 resource "aws_vpc_security_group_egress_rule" "app_out_https" {
   security_group_id = aws_security_group.app.id
